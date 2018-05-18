c2f914d2f648c94c251de12b638037cc77f8d767
Michael Vorburger <mike@vorburger.ch>
2018-05-18 11:43:54 +0200

    add Test.java JUST to make sure compilation works (that's the problem..)

---
+++ src/test/java/com/example/minecraft/test/Test.java
@@ -0,0 +1,12 @@
+package com.example.minecraft.test;
+
+import com.example.minecraft.MainVerticle;
+import com.example.minecraft.Minecraft;
+
+public class Test {
+
+    MainVerticle mainVerticle;
+    Minecraft minecraft;
+
+}
+
